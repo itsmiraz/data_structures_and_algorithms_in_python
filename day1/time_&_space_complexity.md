06f9d08bab32574f795dce0c869e7ac8a18b547d
itsmiraz <mirajulislamofficial044@gmail.com>
2025-12-11 09:55:41 +0600

    new changes

---
+++ day1/time_&_space_complexity.md
@@ -0,0 +1,38 @@
+# Time and Space Complexity
+
+# Time complexity
+
+Time complexity measures the running time of an algorithm as the the input size grows : 2 \* n (n is the input size)
+
+# Big o notation
+
+1. Always calculate tc in terms of worst cases
+2. Avoid the constant values
+3. Avoid lower bound
+
+Different types of Time complexity
+
+Big - On(o)
+|
+Worst Case
+{upper bound}
+
+theta(θ)
+|
+Average Case
+
+Omega (ω)
+|
+Best Case
+
+{lower bound}
+
+<!--  Space complexity -->
+
+= > Memory Space
+
+Big o notation
+
+Auxiliary Space : the extra space used to the solve the problem
+
+- Input Space : the space used to store the input
---
+++ time_complexity_code.py
@@ -0,0 +1,39 @@
+
+#
+# 1. Always calculate tc in the worst case simply that means add any input that will run throw the entire code so that it run all the operations posibile
+# age = 10
+
+# if age >= 80:
+#     print('OLD')
+# elif age >= 60:
+#     print('Senior')
+# elif age >= 24 and age < 60:
+#     print('Working')
+# elif age >= 16 and age < 24:
+#     print('TEEN')
+# else:
+#     print('Baby')
+
+
+# 2. Avoid constant values
+#  TC o()
+
+
+# Example
+
+# n = 10000
+
+# for i in range(1, n+1):
+#     for j in range(1, n + 1):
+#         print('here', i)
+
+
+n = 1
+
+# for i in [1, 2]:
+#     print(i)
+arr = range(1, n+1)
+print(arr)
+
+# for j in range(1, i+1):
+#     print('Something')
